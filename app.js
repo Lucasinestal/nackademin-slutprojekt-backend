efe10eaef1565c3dd06a386393882b1b9c4c4119
faboosh <fabulo1998@gmail.com>
2020-09-18 15:32:56 +0200

    added boiler plate for controllers, models, routes

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
+const orderRouter = require('./routers/orderRouter')
+const productRouter = require('./routers/productRouter')
+const userRouter = require('./routers/userRouter')
 
 app.use( express.static('public') )
+
+//Routers
+app.use('/api', userRouter)
+app.use('/api/orders', orderRouter)
+app.use('/api/products', productRouter)
 
 module.exports = app
