172a956977e7131967983e14c5b07551e5f7ac86
faboosh <fabulo1998@gmail.com>
2020-09-23 12:00:40 +0200

    fixed response formatting when creating producty

--- controllers/productController.js
+++ controllers/productController.js
@@ -36,7 +36,7 @@ async function createProduct(req, res){
 
             const createdProduct = await productModel.createProduct(newProduct);
             console.log('Created product', createdProduct);
-            res.status(201).json(createdProduct);
+            res.status(201).json({product: createdProduct});
         }
     } catch(err){
         console.error(err)
